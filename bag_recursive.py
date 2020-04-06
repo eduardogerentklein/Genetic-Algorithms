052247308bedaa840bfcce6543004641a5e671eb
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2020-04-06 19:47:13 -0300

    updating bag recursive

--- bag_recursive.py
+++ bag_recursive.py
@@ -1,30 +1,29 @@
-WEIGHT_LIMIT = 30
-valor  = [15, 7, 10, 5, 8, 17]
-peso = [15, 3, 2, 5, 9, 20]
+maxWeight = 30
+value  = [15, 7, 10, 5, 8, 17]
+weight = [15, 3, 2, 5, 9, 20]
 
-def knapsack(pos, selected):
-
+def bag(pos, selected):
 	# calcula o total
 	totalValue = 0 
-	totalWeight = 0
-	
-	for i in selected:
-		totalValue += valor[i]
-		totalWeight += peso[i]
+	pesoTotal = 0
 
-	if totalWeight > WEIGHT_LIMIT:
+	for i in selected:
+		totalValue += value[i]
+		pesoTotal += weight[i]
+
+	if pesoTotal > maxWeight:
 		return (0,0)
 		
-	if pos >= len(peso):
-		return (totalValue, totalWeight)
+	if pos >= len(weight):
+		return (totalValue, pesoTotal)
 	
-	answer1 = knapsack(pos+1, selected+[pos])
-	answer2 = knapsack(pos+1, list(selected))
+	answer1 = bag(pos + 1, selected + [pos])
+	answer2 = bag(pos + 1, list(selected))
 	
 	if answer1[0] > answer2[0]:
 		return answer1
 	else:
 		return answer2
 		
-bestAnswer = knapsack(0, [])
+bestAnswer = bag(0, [])
 print(bestAnswer)
